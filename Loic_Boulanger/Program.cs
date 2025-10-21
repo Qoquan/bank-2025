e146f4caeb5a23482cb3bd2fe6c2871553a1ce03
Qoquan <boulangerloic1@gmail.com>
2025-10-21 10:41:05 +0200

    Commit 2.2

--- Loic_Boulanger/Program.cs
+++ Loic_Boulanger/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Loic_Boulanger.Domaine;
+using LoïcBoulanger2.Domain;
 
-Console.WriteLine("Hello, World!");
+var owner = new Person("Alice", "Durand");
+var account = new CurrentAccount("12345", owner, 500); // Découvert de 500 €
+
+account.Deposit(1000);
+account.Withdraw(1200);  // OK → solde = -200
+account.Withdraw(400);   // REFUSÉ → dépasserait le découvert
